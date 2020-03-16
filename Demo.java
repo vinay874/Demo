3d11ed25c3d8f497cd24ba33ae6c60f64e1550b7
vinay874 <40137534+vinay874@users.noreply.github.com>
2020-03-16 09:51:29 +0530

    Update Demo.java
    modified

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 package com.git.demo;
 public class Demo{
   public static void main(String[] args){
-    System.out.println("welcome to git learn program");
+    System.out.println("welcome to git tool learn program");
     }
 }
